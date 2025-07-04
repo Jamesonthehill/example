c174de3d749f85f90136c8df91c47ce845ad008e
geonwoolee <dlrjsdn53@naver.com>
2025-07-05 01:35:49 +0900

    commit testment1

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,7 @@ public class Main {
             System.out.println("I can do it2");
             System.out.println("I am having pain I can do it.");
             System.out.println("다 똑같이 하는 노력은, 노력이 아니다.");
+            System.out.println("천장강대임어사인야");
         }
     }
 }
