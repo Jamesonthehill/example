55db2f1dceb5e7b10570a4f55ead9a2351f27479
geonwoolee <dlrjsdn53@naver.com>
2025-07-16 15:09:04 +0900

    commit testment1

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,7 @@ public class Main {
             System.out.println("do something.");
             System.out.println("비자인터뷰 과정 끝!");
             System.out.println("what is next stage?");
+            System.out.println("·− ···− −−− ·· −··   ··· · −··−");
         }
     }
 }
