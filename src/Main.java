ad6f49ea316dba90dd28cca591cbda5b2dabb2df
geonwoolee <dlrjsdn53@naver.com>
2025-07-22 00:03:53 +0900

    commit testment1

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,7 @@ public class Main {
             System.out.println("I felt that gut instinct to do ");
             System.out.println("which is things that matter most to you?");
             System.out.println("This is not a joke, it's a warning call ");
-
+            System.out.println("Digital Detox");
         }
     }
 }
