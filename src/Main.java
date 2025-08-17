a66815c0cb1c377792c9a0fd6744a7bbccaf8b16
geonwoolee <dlrjsdn53@naver.com>
2025-08-17 14:32:37 -0400

    commit testment1

--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,7 @@ public class Main {
             System.out.println("Digital Detox");
             System.out.println("Figma IPO");
             System.out.println("We bought plane ticket.");
-
+            System.out.println("On August 17.");
         }
     }
 }
