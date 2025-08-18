de6c4ffd4cf145cbd4849f26c4a2a1c9c90e8103
geonwoolee <dlrjsdn53@naver.com>
2025-08-18 00:12:10 -0400

    weeds start

--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,7 @@ public class Main {
             System.out.println("Digital Detox");
             System.out.println("Figma IPO");
             System.out.println("Figma IPO111dfsdfs");
-            System.out.println("Figma IPO111dfsdfs");
+            System.out.println("it works");
         }
     }
 }
