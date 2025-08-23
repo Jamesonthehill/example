880f3f9d9b709aa22d307af092af0416ec75174d
geonwoolee <dlrjsdn53@naver.com>
2025-08-23 11:43:35 -0400

    weeds start 8/23

--- src/Main.java
+++ src/Main.java
@@ -49,6 +49,7 @@ public class Main {
             System.out.println("How long have you been here?");
             System.out.println("How do you like it here so far, How long have you been in Charlotte, How long have you been studying on the library.");
             System.out.println("first try of my resume.");
+            System.out.println("first try of my resume.");
         }
     }
 }
