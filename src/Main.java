2b7c03b7be4355c2ed9a87f4bd5635d8ed9b5275
geonwoolee <dlrjsdn53@naver.com>
2025-09-23 19:37:14 -0400

    weeds start 09/24

--- src/Main.java
+++ src/Main.java
@@ -59,7 +59,7 @@ public class Main {
             System.out.println("DBMS, Machine learning, and AI");
             System.out.println("Let's go to the pycharm");
             System.out.println("Let's go to the pycharm");
-
+            System.out.println("Let's go to the pycharm");
         }
     }
 }
