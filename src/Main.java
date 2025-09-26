3466401cb176d804afd9d1daf5cc2df4048d02db
geonwoolee <dlrjsdn53@naver.com>
2025-09-25 23:08:04 -0400

    weeds start 09/25

--- src/Main.java
+++ src/Main.java
@@ -6,60 +6,9 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            System.out.println(i);
-            System.out.println("i = " + i);
-            System.out.println("i = " + i);
-            System.out.println("i = " + i);
 
-            System.out.println("AI Healthcare");
-            System.out.println("AI Industry");
-            System.out.println("AI in Automation");
-            System.out.println("AI Music");
-            System.out.println("AI Sports");
-            System.out.println("AI Guitar");
-            System.out.println("AI IonQ");
-            System.out.println("AI Palantir");
-            System.out.println("AI 두산에너지빌스");
-            System.out.println("AI Rocket Labs");
-            System.out.println("I can do it");
-            System.out.println("I can do it2");
-            System.out.println("I am having pain I can do it.");
-            System.out.println("다 똑같이 하는 노력은, 노력이 아니다.");
-            System.out.println("천장강대임어사인야");
-            System.out.println("don't suspect yourself.");
-            System.out.println("don't suspect yourself. I can do it");
-            System.out.println("I can take a green card and I believe it.");
-            System.out.println("one day left");
-            System.out.println("I am green card");
-            System.out.println("I got the green card!");
-            System.out.println("god said let there be light!");
-            System.out.println("do something.");
-            System.out.println("비자인터뷰 과정 끝!");
-            System.out.println("what is next stage?");
-            System.out.println("·− ···− −−− ·· −··   ··· · −··−");
-            System.out.println("I felt that gut instinct to do ");
-            System.out.println("which is things that matter most to you?");
-            System.out.println("This is not a joke, it's a warning call ");
-            System.out.println("Digital Detox");
-            System.out.println("Figma IPO");
-            System.out.println("Figma IPO111dfsdfs");
-            System.out.println("it works");
-            System.out.println("I drop the data mining class.");
-            System.out.println("How long have you been here?");
-            System.out.println("How do you like it here so far, How long have you been in Charlotte, How long have you been studying on the library.");
-            System.out.println("first try of my resume.");
-            System.out.println("I finished my resume.");
-            System.out.println("Jupyter notebook is like umbellra to explain code that we made and build.");
-            System.out.println("we got 5 more minute.");
-            System.out.println("we got 24more minute.");
-            System.out.println("Today is Aug 28");
-            System.out.println("DBMS, Machine learning, and AI");
 
-            System.out.println("DBMS, Machine learning, and AI");
-            System.out.println("Let's go to the pycharm");
-            System.out.println("Let's go to the pycharm");
-            
+
         }
     }
 }
